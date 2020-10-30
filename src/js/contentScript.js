79a72f6c3d0aae41bc6a3bf22471bd37a266200a
cwernert <55778578+cwernert@users.noreply.github.com>
2020-10-30 16:22:51 +1100

    Adding copy-paste-card-content function
    Commented out some lines related to closing the Guru Card
    Changed some variable titles
    Enabled separation of two commands with if statement; copy and copy+paste

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -1,16 +1,19 @@
-const guruCardSidebar = document.getElementsByClassName("ghq-CardHomeSidebarCopySection__menu-item ghq-clickable");
-const guruCardTopbar = document.getElementsByClassName("ghq-gsvg ghq-CardHeaderRightSection__item ghq-clickable");
+const guruCopy = document.getElementsByClassName("ghq-CardHomeSidebarCopySection__menu-item ghq-clickable");
+//const guruClose = document.getElementsByClassName("ghq-gsvg ghq-CardHeaderRightSection__item ghq-clickable");
 const helpScoutTicketReply = document.getElementsByClassName('redactor_redactor redactor_editor');
 
-if (guruCardSidebar && guruCardTopbar && helpScoutTicketReply) {
-  chrome.runtime.onMessage.addListener(function (message) {
-    // Click the copy content button...
-    guruCardSidebar[0].click();
-    // Click the close card button...
-    guruCardTopbar[1].click();
-    // Set focus to HS Reply box...
-    helpScoutTicketReply[0].focus();
-    // Paste clipboard content into the HS Reply box...
-    document.execCommand('paste');
-  });
+if (guruCopy && helpScoutTicketReply) {
+	chrome.runtime.onMessage.addListener(function (message) {
+		// Click the copy content button...
+		guruCopy[0].click();
+		// Click the close card button...
+		//guruCardTopbar[1].click();
+		//if the command included paste instruction
+		if(message.command=="copy-paste-card-content"){
+			// Set focus to HS Reply copy-card-contentbox...
+			helpScoutTicketReply[0].focus();
+			// Paste clipboard content into the HS Reply box...
+			document.execCommand('paste');
+		}
+	});
 }
