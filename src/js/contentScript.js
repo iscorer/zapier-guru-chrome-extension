90eddbb56441325972cffcea7d73645638e6cbbb
cwernert <55778578+cwernert@users.noreply.github.com>
2020-11-02 16:02:54 +1100

    Update contentScript.js
    Added a new function which improves the .focus() method we were using previously.
    It ensures that the focus is placed at the end of the HelpScout reply, so that the clip is pasted in the right spot, and the user can continue typing immediately after a paste

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -7,20 +7,40 @@ if (guruCardCopy && guruCardClose && helpScoutTicketReply) {
 		// Try accessing the Guru Extension in the shadow DOM...
 		const guruExtensionClose = document.querySelector("guru-shadow").shadowRoot.querySelector("div.ghq-ExtensionHeader > span.ghq-clickable:last-child");		
 		// Click the copy content button...
-		guruCardCopy[0].click();				
+		guruCardCopy[0].click();
+		// Add some delay to give the Guru extension time to display the copy notification...				
 		setTimeout(function(){
-			// Add some delay to give the Guru extension time to display the copy notification...
 			// Click the close card button...
 			guruCardClose[1].click();
 			// Click the close extension button..
 			guruExtensionClose.click();	
 			// Set focus to HS Reply box...
-			helpScoutTicketReply[0].focus();			
+			focusToEnd(helpScoutTicketReply[0]);
 			//if the command included paste instruction
 			if(message.command=="copy-paste-card-content"){
 				// Paste clipboard content into the HS Reply box...
 				document.execCommand('paste');
+				// Set focus to HS Reply box...
+				focusToEnd(helpScoutTicketReply[0]);
 			}						
-		}, 500); 		
+		}, 500);
 	});
+}
+
+// Finds the end of the HelpScout reply and moves the focus there
+function focusToEnd(el) {
+	el.focus();
+	if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
+		var range = document.createRange();
+		range.selectNodeContents(el);
+		range.collapse(false);
+		var sel = window.getSelection();
+		sel.removeAllRanges();
+		sel.addRange(range);
+	} else if (typeof document.body.createTextRange != "undefined") {
+		var textRange = document.body.createTextRange();
+		textRange.moveToElementText(el);
+		textRange.collapse(false);
+		textRange.select();
+	}
 }
