341415009f8a8022ffd2868436069dfeedfdebe5
Ian Scorer <ian.scorer@gmail.com>
2020-11-02 08:53:11 +0700

    Implement auto-close of Guru with delay
    - Both copy & copy-paste commands auto-close Guru
    - Added a delay to allow Guru to show notification

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -1,19 +1,26 @@
-const guruCopy = document.getElementsByClassName("ghq-CardHomeSidebarCopySection__menu-item ghq-clickable");
-//const guruClose = document.getElementsByClassName("ghq-gsvg ghq-CardHeaderRightSection__item ghq-clickable");
+const guruCardCopy = document.getElementsByClassName("ghq-CardHomeSidebarCopySection__menu-item ghq-clickable");
+const guruCardClose = document.getElementsByClassName("ghq-gsvg ghq-CardHeaderRightSection__item ghq-clickable");
 const helpScoutTicketReply = document.getElementsByClassName('redactor_redactor redactor_editor');
 
-if (guruCopy && helpScoutTicketReply) {
+if (guruCardCopy && guruCardClose && helpScoutTicketReply) {
 	chrome.runtime.onMessage.addListener(function (message) {
+		// Try accessing the Guru Extension in the shadow DOM...
+		const guruExtensionClose = document.querySelector("guru-shadow").shadowRoot.querySelector("div.ghq-ExtensionHeader > span.ghq-clickable:last-child");		
 		// Click the copy content button...
-		guruCopy[0].click();
-		// Click the close card button...
-		//guruCardTopbar[1].click();
-		//if the command included paste instruction
-		if(message.command=="copy-paste-card-content"){
-			// Set focus to HS Reply copy-card-contentbox...
-			helpScoutTicketReply[0].focus();
-			// Paste clipboard content into the HS Reply box...
-			document.execCommand('paste');
-		}
+		guruCardCopy[0].click();				
+		setTimeout(function(){
+			// Add some delay to give the Guru extension time to display the copy notification...
+			// Click the close card button...
+			guruCardClose[1].click();
+			// Click the close extension button..
+			guruExtensionClose.click();	
+			// Set focus to HS Reply box...
+			helpScoutTicketReply[0].focus();			
+			//if the command included paste instruction
+			if(message.command=="copy-paste-card-content"){
+				// Paste clipboard content into the HS Reply box...
+				document.execCommand('paste');
+			}						
+		}, 500); 		
 	});
 }
